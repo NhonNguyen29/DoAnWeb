33e20a21cda77a5b6d68cf96e8645d2b2c39a4a3
KaiOzu284 <nguyentranhoa322@gmail.com>
2023-04-04 15:42:40 +0700

    Xong: QL hãng, Phụ kiện, thương hiệu , đơn hàng. Chưa: Lỗi không lưu được sửa sản phẩm.

--- Webphukien/Controllers/AdminController.cs
+++ Webphukien/Controllers/AdminController.cs
@@ -31,7 +31,30 @@ namespace Webphukien.Controllers
             int pageSize = 6;
             return View(db.SANPHAMs.ToList().OrderBy(n => n.MaSP).ToPagedList(pageNumber,pageSize));
         }
-      
+        public ActionResult QLHDT(int ? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 6;
+            return View(db.HANGDIENTHOAIs.ToList().OrderBy(n => n.MaHDT).ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult QLPK(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 6;
+            return View(db.LOAIPHUKIENs.ToList().OrderBy(n => n.MaLPK).ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult QLTH(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 6;
+            return View(db.THUONGHIEUs.ToList().OrderBy(n => n.MaTH).ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult QLDH(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 6;
+            return View(db.DONDATHANGs.ToList().OrderBy(n => n.MaDonHang).ToPagedList(pageNumber, pageSize));
+        }
         [HttpGet]
         public ActionResult Login()
         {
@@ -65,7 +88,59 @@ namespace Webphukien.Controllers
             }
             return View();
         }
-      
+        // THEM
+        [HttpGet]
+        public ActionResult ThemHDT()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemHDT(HANGDIENTHOAI hdt)
+        {
+            db.HANGDIENTHOAIs.InsertOnSubmit(hdt);
+            db.SubmitChanges();
+            return RedirectToAction("QLHDT");
+        }
+        [HttpGet]
+        public ActionResult ThemLPK()
+        {
+            ViewBag.MaHDT = new SelectList(db.HANGDIENTHOAIs.ToList().OrderBy(n => n.TenHDT), "MaHDT", "TenHDT");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemLPK(LOAIPHUKIEN pk)
+        {
+            ViewBag.MaHDT = new SelectList(db.HANGDIENTHOAIs.ToList().OrderBy(n => n.TenHDT), "MaHDT", "TenHDT");
+            db.LOAIPHUKIENs.InsertOnSubmit(pk);
+            db.SubmitChanges();
+            return RedirectToAction("QLPK");
+        }
+        [HttpGet]
+        public ActionResult ThemTH()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemTH(THUONGHIEU th)
+        {
+            db.THUONGHIEUs.InsertOnSubmit(th);
+            db.SubmitChanges();
+            return RedirectToAction("QLTH");
+        }
+        [HttpGet]
+        public ActionResult ThemDH()
+        {
+            ViewBag.MaKH = new SelectList(db.KHACHHANGs.ToList().OrderBy(n => n.HoTen), "MaKH", "HoTen");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemDH(DONDATHANG dh)
+        {
+            ViewBag.MaKH = new SelectList(db.KHACHHANGs.ToList().OrderBy(n => n.HoTen), "MaKH", "HoTen");
+            db.DONDATHANGs.InsertOnSubmit(dh);
+            db.SubmitChanges();
+            return RedirectToAction("QLDH");
+        }
         [HttpGet]
         public ActionResult Themmoi()
         {
@@ -107,6 +182,7 @@ namespace Webphukien.Controllers
                 return RedirectToAction("Sanpham");
             }
         }
+        //CHI TIET
         public ActionResult Chitiet(int id)
         {
             SANPHAM sp = db.SANPHAMs.SingleOrDefault(n => n.MaSP == id);
@@ -118,6 +194,123 @@ namespace Webphukien.Controllers
             }
             return View(sp);
         }
+        public ActionResult ChitietDH(int id)
+        {
+            DONDATHANG dh = db.DONDATHANGs.SingleOrDefault(n => n.MaDonHang == id);
+            ViewBag.MaDonHang = dh.MaDonHang;
+            if (dh == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(dh);
+        }
+        // XOA
+        public ActionResult XoaHDT(int id)
+        {
+            HANGDIENTHOAI hdt = db.HANGDIENTHOAIs.SingleOrDefault(n => n.MaHDT == id);
+            ViewBag.MaHDT = hdt.MaHDT;
+            if (hdt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(hdt);
+        }
+        [HttpPost, ActionName("XoaHDT")]
+        public ActionResult XacNhanXoaHDT(int id)
+        {
+            HANGDIENTHOAI hdt = db.HANGDIENTHOAIs.SingleOrDefault(n => n.MaHDT == id);
+            ViewBag.MaHDT = hdt.MaHDT;
+            if (hdt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.HANGDIENTHOAIs.DeleteOnSubmit(hdt);
+            db.SubmitChanges();
+            return RedirectToAction("QLHDT");
+        }
+        public ActionResult XoaLPK(int id)
+        {
+            LOAIPHUKIEN pk = db.LOAIPHUKIENs.SingleOrDefault(n => n.MaLPK == id);
+            ViewBag.MaLPK = pk.MaLPK;
+            if (pk == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(pk);
+        }
+        [HttpPost, ActionName("XoaLPK")]
+        public ActionResult XacNhanXoaLPK(int id)
+        {
+
+            LOAIPHUKIEN pk = db.LOAIPHUKIENs.SingleOrDefault(n => n.MaLPK == id);
+            ViewBag.MaLPK = pk.MaLPK;
+            if (pk == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.LOAIPHUKIENs.DeleteOnSubmit(pk);
+            db.SubmitChanges();
+            return RedirectToAction("QLPK");
+        }
+        public ActionResult XoaTH(int id)
+        {
+           THUONGHIEU th = db.THUONGHIEUs.SingleOrDefault(n => n.MaTH == id);
+            ViewBag.MaTH = th.MaTH;
+            if (th == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(th);
+        }
+        [HttpPost, ActionName("XoaTH")]
+        public ActionResult XacNhanXoaTH(int id)
+        {
+
+            THUONGHIEU th = db.THUONGHIEUs.SingleOrDefault(n => n.MaTH == id);
+            ViewBag.MaTH = th.MaTH;
+            if (th == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.THUONGHIEUs.DeleteOnSubmit(th);
+            db.SubmitChanges();
+            return RedirectToAction("QLTH");
+        }
+
+        public ActionResult XoaDH(int id)
+        {
+            DONDATHANG dh = db.DONDATHANGs.SingleOrDefault(n => n.MaDonHang == id);
+            ViewBag.MaDonHang = dh.MaDonHang;
+            if (dh == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(dh);
+        }
+        [HttpPost, ActionName("XoaDH")]
+        public ActionResult XacNhanXoaDH(int id)
+        {
+
+            DONDATHANG dh = db.DONDATHANGs.SingleOrDefault(n => n.MaDonHang == id);
+            ViewBag.MaDonHang = dh.MaDonHang;
+            if (dh == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.DONDATHANGs.DeleteOnSubmit(dh);
+            db.SubmitChanges();
+            return RedirectToAction("QLDH");
+        }
+
         public ActionResult Xoa(int id)
         {
             SANPHAM sp =db.SANPHAMs.SingleOrDefault(n => n.MaSP == id);
@@ -143,6 +336,96 @@ namespace Webphukien.Controllers
             db.SubmitChanges();
             return RedirectToAction("Sanpham");
         }
+        // SUA
+
+        [HttpGet]
+        public ActionResult SuaHDT(int id)
+        {
+            HANGDIENTHOAI hdt = db.HANGDIENTHOAIs.SingleOrDefault(n => n.MaHDT == id);
+            if(hdt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(hdt);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult SuaHDT(int id, HANGDIENTHOAI hdt)
+        {
+            hdt = db.HANGDIENTHOAIs.SingleOrDefault(n => n.MaHDT == id);
+            UpdateModel(hdt);
+            db.SubmitChanges();
+            return RedirectToAction("QLHDT");
+        }
+
+
+        [HttpGet]
+        public ActionResult SuaLPK(int id)
+        {
+            LOAIPHUKIEN pk = db.LOAIPHUKIENs.SingleOrDefault(n => n.MaLPK == id);
+            if (pk == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            ViewBag.MaHDT = new SelectList(db.HANGDIENTHOAIs.ToList().OrderBy(n => n.TenHDT), "MaHDT", "TenHDT");
+            return View(pk);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult SuaLPK(int id, LOAIPHUKIEN pk)
+        {
+            ViewBag.MaHDT = new SelectList(db.HANGDIENTHOAIs.ToList().OrderBy(n => n.TenHDT), "MaHDT", "TenHDT");
+            pk = db.LOAIPHUKIENs.SingleOrDefault(n => n.MaLPK == id);
+            UpdateModel(pk);
+            db.SubmitChanges();
+            return RedirectToAction("QLPK");
+        }
+
+        public ActionResult SuaTH(int id)
+        {
+            THUONGHIEU th = db.THUONGHIEUs.SingleOrDefault(n => n.MaTH == id);
+            if (th == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(th);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult SuaTH(int id, THUONGHIEU th)
+        {
+            th = db.THUONGHIEUs.SingleOrDefault(n => n.MaTH == id);
+            UpdateModel(th);
+            db.SubmitChanges();
+            return RedirectToAction("QLTH");
+        }
+
+
+        public ActionResult SuaDH(int id)
+        {
+            DONDATHANG dh = db.DONDATHANGs.SingleOrDefault(n => n.MaDonHang == id);
+            if (dh == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            ViewBag.MaKH = new SelectList(db.KHACHHANGs.ToList().OrderBy(n => n.HoTen), "MaKH", "HoTen");
+            return View(dh);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult SuaDH(int id, DONDATHANG dh)
+        {
+            ViewBag.MaKH = new SelectList(db.KHACHHANGs.ToList().OrderBy(n => n.HoTen), "MaKH", "HoTen");
+            dh = db.DONDATHANGs.SingleOrDefault(n => n.MaDonHang == id);
+            UpdateModel(dh);
+            db.SubmitChanges();
+            return RedirectToAction("QLDH");
+        }
+
         [HttpGet]
         public ActionResult Sua(int id)
         {
