2dd9fbb6f431355e5cefc95dbaf6dba5ebc61a3a
Noob299 <nhonnguyen2901@gmail.com>
2023-04-11 04:18:03 +0700

    Ok---Letsgooo

--- Webphukien/Controllers/HomeController.cs
+++ Webphukien/Controllers/HomeController.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using System.Xml.Schema;
 using Webphukien.Models;
+
+using PagedList;
+using PagedList.Mvc;
+using System.Net.NetworkInformation;
 
 namespace Webphukien.Controllers
 {
     public class HomeController : Controller
     {
         DangkyDataContext db = new DangkyDataContext();
-        public ActionResult Index(/*int MaSP=0, string TimTuKhoa=""*/)
+        
+        public ActionResult Index(int ? page)
         {
-            return View();
-        } /*return View(db.SANPHAMs.Where(x => x.TenSP.Contains(TimTuKhoa) || TimTuKhoa == null).ToList());*/
+           
+            int pageSize = 3;
+            int pageNum = (page ?? 1);
+            var giacaotoithap = laygiasanpham(10);
+            return View(giacaotoithap.ToPagedList(pageNum, pageSize));
+        } 
 
         public ActionResult About()
         {
@@ -29,5 +39,22 @@ namespace Webphukien.Controllers
 
             return View();
         }
+
+
+        private List<SANPHAM> laygiasanpham(int count)
+        {
+            // sap xep dan theo gia
+            return db.SANPHAMs.OrderByDescending(gia => gia.Giaban).Take(count).ToList();
+
+        }
+
+
+        public ActionResult SanPhamTheoPhuKien(int id)
+        {
+            var sppk = from sp in db.SANPHAMs where sp.MaLPK == id select sp;
+            return View(sppk);
+        }
+
+       
     }
 }
--- Webphukien/Controllers/WebPhuKienController.cs
+++ Webphukien/Controllers/WebPhuKienController.cs
@@ -9,30 +9,36 @@ using Webphukien.Models;
 
 using PagedList;
 using PagedList.Mvc;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+
 namespace Webphukien.Controllers
 {
+
     public class WebPhuKienController : Controller
     {
         DangkyDataContext db = new DangkyDataContext();
-       
+        public ActionResult Index(int? page)
+        {
+         
+                int pageSize = 3;
+                int pageNum = (page ?? 1);
+
+
+
+                ///// lay 5 sp gia cao nhat
+                var giacaotoithap = laygiasanpham(10);
+                return View(giacaotoithap.ToPagedList(pageNum, pageSize));
+            
+         
+        }
         private List<SANPHAM> laygiasanpham(int count)
         {
             // sap xep dan theo gia
             return db.SANPHAMs.OrderByDescending(gia => gia.Giaban).Take(count).ToList();
 
         }
-        public ActionResult Index(int ? page)
-        {
-
-            int pageSize = 6;
-            int pageNum = (page ?? 1);
-
-
-
-            ///// lay 5 sp gia cao nhat
-            var giacaotoithap = laygiasanpham(5);
-            return View(giacaotoithap.ToPagedList(pageNum,pageSize));
-        }
+      
         public ActionResult cacloaiphukien()
         {
             var loaiphukien = from TenPhuKien in db.LOAIPHUKIENs select TenPhuKien; 
@@ -64,5 +70,20 @@ namespace Webphukien.Controllers
             var sanpham = from sp in db.SANPHAMs where sp.MaSP == id select sp;
             return View(sanpham.Single());
         }
+
+        public ActionResult Search(string searchString)
+        {
+
+            var links = from l in db.SANPHAMs
+                        select l;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                links = links.Where(s => s.TenSP.Contains(searchString));
+            }
+
+            return View(links);
+        }
+
     }
 }
